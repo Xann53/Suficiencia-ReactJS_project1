422102f7ac03f0541db4fd45c9e14e05a7019f9e
Xannetz Suficiencia <xvsuficiencia@gmail.com>
2023-04-26 21:11:24 +0800

    Created my first React Project

---
+++ src/pages/homePage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './homePage.css';
+
+class Home extends Component {
+    render() { 
+        return (
+            <div className='Home-header'>
+                <h1>HOMEPAGE</h1>
+                <p>This is a sample homepage by Xannetz Suficiencia</p>
+            </div>
+        );
+    }
+}
+ 
+export default Home;
